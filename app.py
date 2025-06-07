b9299efb93f793efa82f76faedd72cbc1558aef1
ywezj <ywezj@icloud.com>
2025-06-07 19:42:38 +0300

    deleted:    README.md 	new file:   app.py renamed:    instance/project.db -> games.db 	new file:   instance/games.db 	deleted:    main.py 	deleted:    static/store.css 	new file:   static/style.css 	deleted:    static/update.js 	new file:   templates/base.html 	modified:   templates/index.html

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'games.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Game(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f'<Game {self.title} ({self.year})>'
+
+# Создаём таблицы при запуске приложения
+with app.app_context():
+    db.create_all()
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        title = request.form['title']
+        year = request.form['year']
+        
+        # Проверка ввода
+        if not title or not year:
+            return "Необходимо заполнить все поля", 400
+            
+        try:
+            year = int(year)
+        except ValueError:
+            return "Год должен быть числом", 400
+            
+        new_game = Game(title=title, year=year)
+        db.session.add(new_game)
+        db.session.commit()
+        return redirect(url_for('index'))
+    
+    games = Game.query.order_by(Game.year.desc()).all()
+    return render_template('index.html', games=games)
+
+if __name__ == '__main__':
+    app.run(debug=True)
